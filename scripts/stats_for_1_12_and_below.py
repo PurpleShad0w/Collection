6c4151a379659e531537103920f5a3b8e15ec36b
Octave <thepurpl3shad0w@gmail.com>
2022-04-10 22:31:34 +0200

    Spaces

--- scripts/stats_for_1_12_and_below.py
+++ scripts/stats_for_1_12_and_below.py
@@ -13,10 +13,12 @@ def stats():
     df.to_csv (r'raw.csv',header=['values'])
     df = pd.read_csv (r'raw.csv')
     df.rename(columns={'Unnamed: 0':'stats'},inplace=True)
+
     # create the necessary dataframes
     df_general = pd.DataFrame(data={'Statistic':'Games quit','Value':0},index=(0,1))
     df_block = pd.DataFrame(data={'Item':'Stone','Mined':0,'Broken':0,'Crafted':0,'Used':0,'Picked Up':0,'Dropped':0},index=(0,1))
     df_entity = pd.DataFrame(data={'Entity':'Creeper','Killed':0,'Killed by':0},index=(0,1))
+
     # find and collect the statistics
     for i in range(len(df)):
         if df.loc[i,'stats'].startswith('stat.mineBlock'):
@@ -62,10 +64,12 @@ def stats():
         statistic = df.loc[i,'stats'].replace('stat.','')
         s = pd.Series([statistic,df.loc[i,'values']],index=['Statistic','Value'])
         df_general = df_general.append(s,ignore_index=True)
+
     # remove unnecessary columns
     df_block = df_block.iloc[2:]
     df_entity = df_entity.iloc[2:]
     df_general = df_general.iloc[2:]
+
     # fuze the various statistics
     df_block = df_block.fillna(0)
     df_block = df_block.groupby(df_block['Item']).aggregate({'Mined':'sum','Broken':'sum','Crafted':'sum','Used':'sum','Picked Up':'sum','Dropped':'sum'})
@@ -73,10 +77,12 @@ def stats():
     df_entity = df_entity.groupby(df_entity['Entity']).aggregate({'Killed':'sum','Killed by':'sum'})
     df_general = df_general.fillna(0)
     df_general = df_general.groupby(df_general['Statistic']).aggregate({'Value':'sum'})
+
     # separate items and mods
     df_block.reset_index(level=0, inplace=True)
     df_block[['Mod', 'Item']] = df_block['Item'].str.split('.', n=1, expand=True)
     df_block = df_block[['Mod','Item','Mined','Broken','Crafted','Used','Picked Up','Dropped']]
+    
     # output the excel file
     writer = pd.ExcelWriter(r'output.xlsx', engine='xlsxwriter')
     df_general.to_excel(writer, sheet_name='General')
--- scripts/stats_for_1_13_and_above.py
+++ scripts/stats_for_1_13_and_above.py
@@ -16,10 +16,12 @@ def stats():
     df.to_csv (r'raw.csv',header=['values'])
     df = pd.read_csv (r'raw.csv')
     df.rename(columns={'Unnamed: 0':'stats'},inplace=True)
+
     # create the necessary dataframes
     df_general = pd.DataFrame(data={'Statistic':'Games quit','Value':0},index=(0,1))
     df_block = pd.DataFrame(data={'Item':'Stone','Mined':0,'Broken':0,'Crafted':0,'Used':0,'Picked Up':0,'Dropped':0},index=(0,1))
     df_entity = pd.DataFrame(data={'Entity':'Creeper','Killed':0,'Killed by':0},index=(0,1))
+
     # find and collect the statistics
     for i in range(len(df)):
         if df.loc[i,'stats'].startswith('stats.minecraft:mined'):
@@ -58,10 +60,12 @@ def stats():
             statistic = re.search('stats.minecraft:custom.(.*).',df.loc[i,'stats'])
             s = pd.Series([statistic.group().replace('stats.minecraft:custom.',''),df.loc[i,'values']],index=['Statistic','Value'])
             df_general = df_general.append(s,ignore_index=True)
+
     # remove unnecessary columns
     df_block = df_block.iloc[2:]
     df_entity = df_entity.iloc[2:]
     df_general = df_general.iloc[2:]
+
     # fuze the various statistics
     df_block = df_block.fillna(0)
     df_block = df_block.groupby(df_block['Item']).aggregate({'Mined':'sum','Broken':'sum','Crafted':'sum','Used':'sum','Picked Up':'sum','Dropped':'sum'})
@@ -69,18 +73,22 @@ def stats():
     df_entity = df_entity.groupby(df_entity['Entity']).aggregate({'Killed':'sum','Killed by':'sum'})
     df_general = df_general.fillna(0)
     df_general = df_general.groupby(df_general['Statistic']).aggregate({'Value':'sum'})
+
     # separate items and mods
     df_block.reset_index(level=0, inplace=True)
     df_block[['Mod', 'Item']] = df_block['Item'].str.split(':', n=1, expand=True)
     df_block = df_block[['Mod','Item','Mined','Broken','Crafted','Used','Picked Up','Dropped']]
+
     # separate entities and mods
     df_entity.reset_index(level=0, inplace=True)
     df_entity[['Mod', 'Entity']] = df_entity['Entity'].str.split(':', n=1, expand=True)
     df_entity = df_entity[['Mod','Entity','Killed','Killed by']]
+
     # separate general statistics and mods
     df_general.reset_index(level=0, inplace=True)
     df_general[['Mod', 'Statistic']] = df_general['Statistic'].str.split(':', n=1, expand=True)
     df_general = df_general[['Mod','Statistic','Value']]
+    
     # output the excel file
     writer = pd.ExcelWriter(r'output.xlsx', engine='xlsxwriter')
     df_general.to_excel(writer, sheet_name='General',index=False)
