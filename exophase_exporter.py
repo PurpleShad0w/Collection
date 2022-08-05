7a5eebbf08406c4b2d3b5bbee91e664229c9a81f
Octave <thepurpl3shad0w@gmail.com>
2022-08-05 03:36:05 +0200

    Fixed number of games issue

--- exophase_exporter.py
+++ exophase_exporter.py
@@ -48,55 +48,68 @@ WebDriverWait(driver,10).until(EC.frame_to_be_available_and_switch_to_it(iframe)
 cookieButton = driver.find_element(By.XPATH,'//*[@id="notice"]/div[3]/button[3]')
 WebDriverWait(driver,10).until(EC.element_to_be_clickable(cookieButton)).click()
 
+# Sort game list
+sortButton = driver.find_element(By.XPATH,'//*[@id="dropdownMenuButton"]')
+sortButton.click()
+alphaButton = driver.find_element(By.XPATH,'//*[@id="app"]/div/div[2]/div[1]/div[1]/div[2]/div/div/a[7]')
+alphaButton.click()
+time.sleep(4)
+
 scroll_to_bottom(driver)
 
-total_games = int(float(driver.find_element(By.XPATH,'//*[@id="sub-user-info"]/section[1]/div[3]/div/div[3]/div/div/span[4]').text))
+# total_games = int(float(driver.find_element(By.XPATH,'//*[@id="sub-user-info"]/section[1]/div[3]/div/div[3]/div/div/span[4]').text))
+i = 0
 
-for i in range(total_games):
-    title_xpath = '//*[@id="app"]/div/div[2]/div[1]/ul/li['+str(i+1)+']/div[1]/div[2]/div/h3/a'
-    title_xpath_alt = '//*[@id="app"]/div/div[2]/div[1]/ul/li['+str(i+1)+']/div[1]/div[2]/div/h3'
-    platform_xpath = '//*[@id="app"]/div/div[2]/div[1]/ul/li['+str(i+1)+']/div[1]/div[2]/div/div'
-    hours_xpath = '//*[@id="app"]/div/div[2]/div[1]/ul/li['+str(i+1)+']/div[1]/div[2]/div/span'
-    achiev_xpath = '//*[@id="app"]/div/div[2]/div[1]/ul/li['+str(i+1)+']/div[1]/div[4]/div[1]/div[1]'
-    date_xpath = '//*[@id="app"]/div/div[2]/div[1]/ul/li['+str(i+1)+']/div[1]/div[5]/div'
+while True:
     try:
-        title = driver.find_element(By.XPATH,title_xpath).text
-    except:
-        title = driver.find_element(By.XPATH,title_xpath_alt).text
-    try:
-        hours = driver.find_element(By.XPATH,hours_xpath).text
-    except:
-        hours = '0h'
-    try:
-        achievements = driver.find_element(By.XPATH,achiev_xpath).text
-    except:
-        achievements = '0/0'
-    try:
-        date = driver.find_element(By.XPATH,date_xpath).text
-    except:
-        date = 'N/A'
-    
-    # Collect platforms
-    j = 0
-    platforms = []
-    while True:
-        j += 1
+        title_xpath = '//*[@id="app"]/div/div[2]/div[1]/ul/li['+str(i+1)+']/div[1]/div[2]/div/h3/a'
+        title_xpath_alt = '//*[@id="app"]/div/div[2]/div[1]/ul/li['+str(i+1)+']/div[1]/div[2]/div/h3'
+        platform_xpath = '//*[@id="app"]/div/div[2]/div[1]/ul/li['+str(i+1)+']/div[1]/div[2]/div/div'
+        hours_xpath = '//*[@id="app"]/div/div[2]/div[1]/ul/li['+str(i+1)+']/div[1]/div[2]/div/span'
+        achiev_xpath = '//*[@id="app"]/div/div[2]/div[1]/ul/li['+str(i+1)+']/div[1]/div[4]/div[1]/div[1]'
+        date_xpath = '//*[@id="app"]/div/div[2]/div[1]/ul/li['+str(i+1)+']/div[1]/div[5]/div'
         try:
-            platform = driver.find_element(By.XPATH,platform_xpath+'/div['+str(j)+']/span').text
-            platforms.append(platform)
+            title = driver.find_element(By.XPATH,title_xpath).text
         except:
-            break
+            title = driver.find_element(By.XPATH,title_xpath_alt).text
+        try:
+            hours = driver.find_element(By.XPATH,hours_xpath).text
+        except:
+            hours = '0h'
+        try:
+            achievements = driver.find_element(By.XPATH,achiev_xpath).text
+        except:
+            achievements = '0/0'
+        try:
+            date = driver.find_element(By.XPATH,date_xpath).text
+        except:
+            date = 'N/A'
     
-    # Cleaning
-    hours = hours[:-1]
-    achiev_obtained, achiev_total = achievements.split('/')
-    date = date.replace(',','')
-    platforms = sorted(platforms)
-    platforms = ' | '.join(platforms)
+        # Collect platforms
+        j = 0
+        platforms = []
+        while True:
+            j += 1
+            try:
+                platform = driver.find_element(By.XPATH,platform_xpath+'/div['+str(j)+']/span').text
+                platforms.append(platform)
+            except:
+                break
     
-    # Append to dataframe
-    s = pd.Series({'Title':title,'Platforms':platforms,'Hours Played':hours,'Achievements Obtained':achiev_obtained,'Achievements Total':achiev_total,'Completion Percentage':0,'Last Played':date})
-    df = df.append(s,ignore_index=True)
+        # Cleaning
+        hours = hours[:-1]
+        achiev_obtained, achiev_total = achievements.split('/')
+        date = date.replace(',','')
+        platforms = sorted(platforms)
+        platforms = ' | '.join(platforms)
+    
+        # Append to dataframe
+        s = pd.Series({'Title':title,'Platforms':platforms,'Hours Played':hours,'Achievements Obtained':achiev_obtained,'Achievements Total':achiev_total,'Completion Percentage':0,'Last Played':date})
+        df = df.append(s,ignore_index=True)
+        i += 1
+    
+    except:
+        break
 
 # Cleaning
 df = df.drop_duplicates()
