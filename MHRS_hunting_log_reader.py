9844dabbb1660ce8c11b1e242cb09eccaa815018
Octave <thepurpl3shad0w@gmail.com>
2022-10-24 19:50:54 +0200

    Added experimental MHRS Hunting Log

---
+++ MHRS_hunting_log_reader.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import pandas as pd
+import warnings
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+os.chdir(os.path.dirname(sys.argv[0]))
+
+import pymem
+
+import data.monsters as monsters
+
+pm = pymem.Pymem('MonsterHunterRise.exe')
+
+# 140000000 + 0xF128600 ?
+
+hunt_pointer = 0x272CACF0
+hunt_pointer = hunt_pointer - 0x4
+capture_pointer = hunt_pointer + 0x420
+anomaly_pointer = hunt_pointer + 0x210
+
+names = monsters.mhrs_names
+small_monsters = monsters.small_monsters
+
+df = pd.DataFrame(data={'ID':0,'Size':0,'Type':0,'Variation':0,'Name':0,'Hunted':0,'Captured':0,'Killed':0,'Anomalies':0,'Big Crown':0,'Small Crown':0,'Largest Size':0,'Smallest Size':0},index=(0,1))
+
+for i in range(len(names)):
+    hunt_pointer = hunt_pointer + 0x4
+    capture_pointer = capture_pointer + 0x4
+    anomaly_pointer = anomaly_pointer + 0x4
+
+    hunts = pm.read_int(hunt_pointer)
+    caps = pm.read_int(capture_pointer)
+    anoms = pm.read_int(anomaly_pointer)
+
+    if names[i] in small_monsters:
+        size = 'Small'
+    elif names[i] not in small_monsters and names[i] != '':
+        size = 'Large'
+    else:
+        size = 'Unknown'
+
+    mtype = monsters.find_monster_type(names[i])
+
+    mvar = monsters.find_monster_variation(names[i])
+
+    kills = hunts - caps
+
+    s = pd.Series({'ID':i,'Size':size,'Type':mtype,'Variation':mvar,'Name':names[i],'Hunted':hunts,'Captured':caps,'Killed':kills,'Anomalies':anoms,'Big Crown':'','Small Crown':'','Largest Size':0,'Smallest Size':0})
+    df = df.append(s, ignore_index=True)
+
+df = df[df['Name'] != 0]
+df = df[df['Size'] != 'Unknown']
+
+df.to_csv(r'outputs/MHRS_hunting_log.csv',encoding='utf-8',index=False)
--- data/monsters.py
+++ data/monsters.py
@@ -42,28 +42,46 @@ mhwi_names = [
     'Yian Garuga','Rajang','Furious Rajang','Brute Tigrex','Zinogre','Stygian Zinogre','Raging Brachydios',"Safi'jiiva",
     '','Scarred Yian Garuga','Frostfang Barioth','Fatalis']
 
+mhrs_names = [
+    'Rathian', 'Apex Rathian', 'Rathalos', 'Apex Rathalos', 'Khezu', 'Basarios', 'Diablos', 'Apex Diablos', 'Rajang', 
+    'Kushala Daora', 'Chameleos', 'Teostra', 'Tigrex', 'Nargacuga', 'Barioth', 'Barroth', 'Royal Ludroth', 'Great Baggi', 
+    'Zinogre', 'Apex Zinogre','Great Wroggi', 'Arzuros', 'Apex Arzuros', 'Lagombi', 'Volvidon', 'Mizutsune', 'Apex Mizutsune', 
+    'Crimson Glow Valstrax', 'Magnamalo', 'Bishaten', 'Aknosom', 'Tetranadon', 'Somnacanth', 'Rakna-Kadaki', 'Almudron', 'Wind Serpent Ibushi', 
+    'Goss Harag', 'Great Izuchi', 'Thunder Serpent Narwa', 'Narwa the Allmother', 'Anjanath', 'Pukei-Pukei', 'Kulu-Ya-Ku', 'Jyuratodus', 'Tobi-Kadachi', 
+    'Bazelgeuse', '', 'Kelbi', '', 'Felyne', 'Melynx', 'Bullfango', 'Popo', 'Anteka', 
+    'Remobra', 'Rhenoplos', 'Bnahabra', 'Altaroth', 'Gajau', 'Jaggi', 'Jaggia', 'Baggi', 'Delex', 
+    'Ludroth', 'Uroktor', 'Slagtoth', 'Gargwa', 'Wroggi', 'Zamite', 'Jagras', 'Kestodon', '', 
+    'Bombadgy', 'Izuchi', '', 'Rachnoid', 'Gold Rathian', 'Silver Rathalos', 'Daimyo Hermitaur', 'Shogun Ceanataur', 'Furious Rajang', 
+    'Lucent Nargacuga', 'Gore Magala', 'Shagaru Magala', 'Seregios', 'Astalos', 'Violet Mizutsune', 'Scorned Magnamalo', 'Blood Orange Bishaten', 'Aurora Somnacanth', 
+    'Pyre Rakna-Kadaki', 'Magma Almudron', 'Seething Bazelgeuse', 'Malzeno', 'Lunagaron', 'Garangolm', 'Gaismagorm', 'Espinas', 'Flaming Espinas', 
+    'Hornetaur', 'Vespoid', 'Velociprey', 'Hermitaur', 'Ceanataur', 'Pyrantula', 'Gowngoat', 'Boggi', '', 
+    'Risen Chameleos'
+]
 
 # List of monsters by size
 
 small_monsters = [
-    'Altaroth', 'Anteka', 'Apceros', 'Aptonoth', 'Barnos', 'Blango', 'Boaboa', 'Bnahabra', 'Bullfango', 
-    'Ceanataur', 'Cephalos', 'Conga', 'Cortos', 'Felyne', 'Gajalaka', 'Gajau', 'Gargwa', 'Gastodon', 
-    'Genprey', 'Giaprey', 'Girros', 'Great Thunderbug', 'Grimalkyne', 'Hermitaur', 'Hornetaur', 'Ioprey', 'Jaggi', 
-    'Jaggia', 'Jagras', 'Kelbi', 'Kelbi (female)', 'Kelbi (male)', 'Kestodon', 'Kestodon (female)', 'Kestodon (male)', 'Konchu', 
-    'Larinoth', 'Ludroth', 'Maccao', 'Melynx', 'Mernos', 'Moofah', 'Mosswine', 'Noios', 'Popo', 
-    'Raphinos', 'Remobra', 'Rhenoplos', 'Shamos', 'Slagtoth', 'Uroktor', 'Velociprey', 'Vespoid', 'Wulg', 
-    'Zamite']
+    'Altaroth', 'Anteka', 'Apceros', 'Aptonoth', 'Baggi', 'Barnos', 'Blango', 'Boaboa', 'Bnahabra', 
+    'Boggi', 'Bombadgy', 'Bullfango', 'Ceanataur', 'Cephalos', 'Conga', 'Cortos', 'Delex', 'Felyne', 
+    'Gajalaka', 'Gajau', 'Gargwa', 'Gastodon',  'Genprey', 'Giaprey', 'Girros', 'Gowngoat', 'Great Thunderbug', 
+    'Grimalkyne', 'Hermitaur', 'Hornetaur', 'Ioprey', 'Izuchi', 'Jaggi', 'Jaggia', 'Jagras', 'Kelbi', 
+    'Kelbi (female)', 'Kelbi (male)', 'Kestodon', 'Kestodon (female)', 'Kestodon (male)', 'Konchu', 'Larinoth', 'Ludroth', 'Maccao', 
+    'Melynx', 'Mernos', 'Moofah', 'Mosswine', 'Noios', 'Popo', 'Pyrantula', 'Rachnoid', 'Raphinos', 
+    'Remobra', 'Rhenoplos', 'Shamos', 'Slagtoth', 'Uroktor', 'Velociprey', 'Vespoid', 'Wroggi', 'Wulg', 
+    'Zamite'
+]
 
 
 # List of monsters by type
 
 type_amphibian = [
-    'Tetsucabra', 'Berserk Tetsucabra', 'Drilltusk Tetsucabra', 'Zamtrios', 'Tigerstripe Zamtrios', 'Zamite']
+    'Tetranadon', 'Tetsucabra', 'Berserk Tetsucabra', 'Drilltusk Tetsucabra', 'Zamtrios', 'Tigerstripe Zamtrios', 'Zamite']
 
 type_bird_wyvern = [
-    'Gargwa', 'Gendrome', 'Genprey', 'Giadrome', 'Giaprey', 'Great Jaggi', 'Great Maccao', 'Gypceros', 'Purple Gypceros', 
-    'Kulu-Ya-Ku', 'Malfestio', 'Nightcloak Malfestio', 'Iodrome', 'Ioprey', 'Jaggi', 'Jaggia', 'Maccao', 'Pukei-Pukei', 
-    'Coral Pukei-Pukei', 'Tzitzi-Ya-Ku', 'Velocidrome', 'Velociprey', 'Yian Garuga', 'Deadeye Yian Garuga', 'Scarred Yian Garuga', 'Yian Kut-Ku', 'Blue Yian Kut-Ku']
+    'Aknosom', 'Baggi', 'Boggi', 'Gargwa', 'Gendrome', 'Genprey', 'Giadrome', 'Giaprey', 'Great Baggi', 
+    'Great Izuchi', 'Great Jaggi', 'Great Maccao', 'Great Wroggi', 'Gypceros', 'Purple Gypceros', 'Kulu-Ya-Ku', 'Malfestio', 'Nightcloak Malfestio', 
+    'Iodrome', 'Ioprey', 'Izuchi', 'Jaggi', 'Jaggia', 'Maccao', 'Pukei-Pukei', 'Coral Pukei-Pukei', 'Tzitzi-Ya-Ku', 
+    'Velocidrome', 'Velociprey', 'Wroggi', 'Yian Garuga', 'Deadeye Yian Garuga', 'Scarred Yian Garuga', 'Yian Kut-Ku', 'Blue Yian Kut-Ku']
 
 type_brute_wyvern = [
     'Anjanath', 'Fulgur Anjanath', 'Banbaro', 'Barroth', 'Brachydios', 'Raging Brachydios', 'Deviljho', 'Savage Deviljho', 'Duramboros', 
@@ -73,29 +91,31 @@ type_carapaceon = [
     'Ceanataur', 'Daimyo Hermitaur', 'Plum Daimyo Hermitaur', 'Stonefist Hermitaur', 'Hermitaur', 'Shogun Ceanataur', 'Rustrazor Ceanataur']
 
 type_fanged_beast = [
-    'Arzuros', 'Redhelm Arzuros', 'Blango', 'Blangonga', 'Bullfango', 'Bulldrome', 'Conga', 'Congalala', 'Emerald Congalala', 
-    'Gammoth', 'Elderfrost Gammoth', 'Kecha Wacha', 'Ash Kecha Wacha', 'Lagombi', 'Snowbaron Lagombi', 'Rajang', 'Furious Rajang', 'Volvidon']
+    'Arzuros', 'Apex Arzuros', 'Redhelm Arzuros', 'Bishaten', 'Blood Orange Bishaten', 'Blango', 'Blangonga', 'Bombadgy', 'Bullfango', 
+    'Bulldrome', 'Conga', 'Congalala', 'Emerald Congalala', 'Gammoth', 'Elderfrost Gammoth', 'Garangolm', 'Goss Harag', 'Kecha Wacha', 
+    'Ash Kecha Wacha', 'Lagombi', 'Snowbaron Lagombi', 'Rajang', 'Furious Rajang', 'Volvidon']
 
 type_fanged_wyvern = [
-    'Dodogama', 'Girros', 'Great Girros', 'Great Jagras', 'Jagras', 'Odogaron', 'Ebony Odogaron', 'Shamos', 'Tobi-Kadachi', 
-    'Viper Tobi-Kadachi', 'Wulg', 'Zinogre', 'Stygian Zinogre', 'Thunderlord Zinogre']
+    'Dodogama', 'Girros', 'Great Girros', 'Great Jagras', 'Jagras', 'Lunagaron', 'Magnamalo', 'Scorned Magnamalo', 'Odogaron', 
+    'Ebony Odogaron', 'Shamos', 'Tobi-Kadachi', 'Viper Tobi-Kadachi', 'Wulg', 'Zinogre', 'Apex Zinogre', 'Stygian Zinogre', 'Thunderlord Zinogre']
 
 type_fish = [
     'Gajau']
 
 type_flying_wyvern = [
     'Akantor', 'Astalos', 'Boltreaver Astalos', 'Barioth', 'Frostfang Barioth', 'Basarios', 'Ruby Basarios', 'Bazelgeuse', 'Seething Bazelgeuse', 
-    'Diablos', 'Black Diablos', 'Bloodbath Diablos', 'Gravios', 'Black Gravios', 'Khezu', 'Red Khezu', 'Legiana', 'Shrieking Legiana', 
-    'Monoblos', 'White Monoblos', 'Nargacuga', 'Silverwind Nargacuga', 'Paolumu', 'Nightshade Paolumu', 'Rathalos', 'Azure Rathalos', 'Dreadking Rathalos', 
-    'Silver Rathalos', 'Rathian', 'Dreadqueen Rathian', 'Gold Rathian', 'Pink Rathian', 'Seregios', 'Tigrex', 'Brute Tigrex', 'Grimclaw Tigrex', 
-    'Molten Tigrex', 'Ukanlos']
+    'Diablos', 'Apex Diablos', 'Black Diablos', 'Bloodbath Diablos', 'Espinas', 'Flaming Espinas', 'Gravios', 'Black Gravios', 'Khezu', 
+    'Red Khezu', 'Legiana', 'Shrieking Legiana', 'Monoblos', 'White Monoblos', 'Nargacuga', 'Lucent Nargacuga', 'Silverwind Nargacuga', 'Paolumu', 
+    'Nightshade Paolumu', 'Rathalos', 'Apex Rathalos', 'Azure Rathalos', 'Dreadking Rathalos', 'Silver Rathalos', 'Rathian', 'Apex Rathian', 'Dreadqueen Rathian', 
+    'Gold Rathian', 'Pink Rathian', 'Seregios', 'Tigrex', 'Brute Tigrex', 'Grimclaw Tigrex', 'Molten Tigrex', 'Ukanlos']
 
 type_herbivore = [
-    'Anteka', 'Apceros', 'Aptonoth', 'Gastodon', 'Kelbi', 'Kelbi (female)', 'Kelbi (male)', 'Kestodon', 'Kestodon (female)', 
-    'Kestodon (male)', 'Larinoth', 'Mosswine', 'Moofah', 'Popo', 'Rhenoplos', 'Slagtoth']
+    'Anteka', 'Apceros', 'Aptonoth', 'Gastodon', 'Gowngoat', 'Kelbi', 'Kelbi (female)', 'Kelbi (male)', 'Kestodon', 
+    'Kestodon (female)', 'Kestodon (male)', 'Larinoth', 'Mosswine', 'Moofah', 'Popo', 'Rhenoplos', 'Slagtoth']
 
 type_leviathan = [
-    'Agnaktor', 'Lagiacrus', 'Ludroth', 'Mizutsune', 'Soulseer Mizutsune', 'Nibelsnarf', 'Royal Ludroth', 'Uroktor']
+    'Agnaktor', 'Almudron', 'Magma Almudron', 'Lagiacrus', 'Ludroth', 'Mizutsune', 'Apex Mizutsune', 'Soulseer Mizutsune', 'Violet Mizutsune', 
+    'Nibelsnarf', 'Royal Ludroth', 'Somnacanth', 'Aurora Somnacanth', 'Uroktor']
 
 type_lynian = [
     'Boaboa', 'Felyne', 'Gajalaka', 'Grimalkyne', 'Melynx']
@@ -114,16 +134,17 @@ type_snake_wyvern = [
     'Najarala', 'Tidal Najarala', 'Remobra']
 
 type_temnoceran = [
-    'Nerscylla', 'Shrouded Nerscylla']
+    'Nerscylla', 'Shrouded Nerscylla', 'Pyrantula', 'Rachnoid', 'Rakna-Kadaki', 'Pyre Rakna-Kadaki']
 
 type_wingdrake = [
     'Barnos', 'Cortos', 'Mernos', 'Noios', 'Raphinos']
 
 type_elder_dragon = [
-    'Alatreon', 'Amatsu', 'Behemoth', 'Chameleos', "Dah'ren Mohran", 'Dalamadur', 'Shah Dalamadur', 'Fatalis', 'Crimson Fatalis', 
-    'White Fatalis', 'Gogmazios', 'Kirin', 'Oroshi Kirin', 'Kulve Taroth', 'Kushala Daora', 'Lao-Shan Lung', 'Lunastra', 'Nakarkos', 
-    'Namielle', 'Nergigante', 'Ruiner Nergigante', "Safi'jiiva", 'Shagaru Magala', 'Shara Ishvalda', 'Teostra', 'Vaal Hazak', 'Blackveil Vaal Hazak', 
-    'Valstrax', 'Velkhana', "Xeno'jiiva", 'Zorah Magdaros']
+    'Alatreon', 'Amatsu', 'Behemoth', 'Chameleos', 'Risen Chameleos', "Dah'ren Mohran", 'Dalamadur', 'Shah Dalamadur', 'Fatalis', 
+    'Crimson Fatalis', 'White Fatalis', 'Gaismagorm', 'Gogmazios', 'Kirin', 'Oroshi Kirin', 'Kulve Taroth', 'Kushala Daora', 'Lao-Shan Lung', 
+    'Lunastra', 'Malzeno', 'Nakarkos', 'Namielle', 'Nergigante', 'Ruiner Nergigante', "Safi'jiiva", 'Shagaru Magala', 'Shara Ishvalda', 
+    'Teostra', 'Thunder Serpent Narwa', 'Narwa the Allmother', 'Vaal Hazak', 'Blackveil Vaal Hazak', 'Valstrax', 'Crimson Glow Valstrax', 'Velkhana', 
+    'Wind Serpent Ibushi', "Xeno'jiiva", 'Zorah Magdaros']
 
 type_question_mark = [
     'Gore Magala', 'Chaotic Gore Magala']
@@ -146,26 +167,34 @@ def find_monster_type(monster_name):
 
 # List of monsters by variation
 
+var_apex =[
+    'Apex Arzuros', 'Apex Diablos', 'Apex Mizutsune', 'Apex Rathalos', 'Apex Rathian', 'Apex Zinogre']
+
 var_deviant = [
     'Redhelm Arzuros', 'Boltreaver Astalos', 'Rustrazor Ceanataur', 'Bloodbath Diablos', 'Elderfrost Gammoth', 'Hellblade Glavenus', 'Stonefist Hermitaur', 
     'Snowbaron Lagombi', 'Nightcloak Malfestio', 'Soulseer Mizutsune', 'Silverwind Nargacuga', 'Dreadking Rathalos', 'Dreadqueen Rathian', 'Drilltusk Tetsucabra', 
     'Grimclaw Tigrex', 'Crystalbeard Uragaan', 'Deadeye Yian Garuga', 'Thunderlord Zinogre']
 
 var_rarespecies = [
-    'Silver Rathalos', 'Gold Rathian', 'Molten Tigrex']
+    'Violet Mizutsune', 'Lucent Nargacuga', 'Silver Rathalos', 'Gold Rathian', 'Molten Tigrex']
+
+var_risen = [
+    'Risen Chameleos']
 
 var_subspecies = [
-    'Fulgur Anjanath', 'Ruby Basarios', 'Emerald Congalala', 'Plum Daimyo Hermitaur', 'Shah Dalamadur', 'Black Diablos', 'Acidic Glavenus', 
-    'Black Gravios', 'Purple Gypceros', 'Ash Kecha Wacha', 'Red Khezu', 'Oroshi Kirin', 'White Monoblos', 'Tidal Najarala', 
-    'Shrouded Nerscylla', 'Ebony Odogaron', 'Nightshade Paolumu', 'Coral Pukei-Pukei', 'Azure Rathalos', 'Pink Rathian', 'Desert Seltas', 
-    'Desert Seltas Queen', 'Berserk Tetsucabra', 'Brute Tigrex', 'Viper Tobi-Kadachi', 'Blue Yian Kut-Ku', 'Tigerstripe Zamtrios', 'Stygian Zinogre']
+    'Magma Almudron', 'Fulgur Anjanath', 'Ruby Basarios', 'Blood Orange Bishaten', 'Emerald Congalala', 'Plum Daimyo Hermitaur', 'Shah Dalamadur', 
+    'Black Diablos', 'Flaming Espinas', 'Acidic Glavenus', 'Black Gravios', 'Purple Gypceros', 'Ash Kecha Wacha', 'Red Khezu', 
+    'Oroshi Kirin', 'White Monoblos', 'Tidal Najarala', 'Shrouded Nerscylla', 'Ebony Odogaron', 'Nightshade Paolumu', 'Coral Pukei-Pukei', 
+    'Pyre Rakna-Kadaki', 'Azure Rathalos', 'Pink Rathian', 'Desert Seltas', 'Desert Seltas Queen', 'Aurora Somnacanth', 'Berserk Tetsucabra', 
+    'Brute Tigrex', 'Viper Tobi-Kadachi', 'Blue Yian Kut-Ku', 'Tigerstripe Zamtrios', 'Stygian Zinogre']
 
 var_variant = [
-    'Frostfang Barioth', 'Seething Bazelgeuse', 'Raging Brachydios', 'Savage Deviljho', 'Chaotic Gore Magala', 'Shrieking Legiana', 'Ruiner Nergigante', 
-    'Furious Rajang', 'Blackveil Vaal Hazak', 'Scarred Yian Garuga']
+    'Frostfang Barioth', 'Seething Bazelgeuse', 'Raging Brachydios', 'Savage Deviljho', 'Chaotic Gore Magala', 'Scorned Magnamalo', 'Narwa the Allmother', 
+    'Shrieking Legiana', 'Ruiner Nergigante', 'Furious Rajang', 'Blackveil Vaal Hazak', 'Crimson Glow Valstrax', 'Scarred Yian Garuga']
 
 monster_variations = {
-    'Deviant': var_deviant, 'Rare Species': var_rarespecies, 'Subspecies': var_subspecies, 'Variant': var_variant}
+    'Apex': var_apex, 'Deviant': var_deviant, 'Rare Species': var_rarespecies, 'Risen': var_risen, 'Subspecies': var_subspecies, 
+    'Variant': var_variant}
 
 
 # Function for finding the monster variation
