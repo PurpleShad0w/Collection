b67301a12ddc436c3a3f9b6d29feedce264eddcf
Octave <thepurpl3shad0w@gmail.com>
2022-11-18 20:53:52 +0100

    Exophase Exporter - added library

--- exophase_exporter.py
+++ exophase_exporter.py
@@ -16,39 +16,31 @@ from selenium.webdriver.support import expected_conditions as EC
 def scroll_to_bottom(driver):
     SCROLL_PAUSE_TIME = 7.5
 
-    # Get scroll height
     last_height = driver.execute_script("return document.body.scrollHeight")
 
     while True:
-        # Scroll down to bottom
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-        # Wait to load page
         time.sleep(SCROLL_PAUSE_TIME)
 
-        # Calculate new scroll height and compare with last scroll height
         new_height = driver.execute_script("return document.body.scrollHeight")
         if new_height == last_height:
             break
         last_height = new_height
 
-
 user = 'PurpleShadow'
 
-df = pd.DataFrame(data={'Title':0,'Region':0,'Platforms':0,'Hours Played':0,'Obtained':0,'Total':0,'Completion':0,'EXP':0,'Points':0,'Platinums':0,'Golds':0,'Silvers':0,'Bronzes':0,'Last Played':0},index=(0,1))
+df = pd.DataFrame(data={'Title':0,'Region':0,'Library':0,'Platforms':0,'Hours Played':0,'Obtained':0,'Total':0,'Completion':0,'EXP':0,'Points':0,'Platinums':0,'Golds':0,'Silvers':0,'Bronzes':0,'Last Played':0},index=(0,1))
 
-# Load page
 driver = webdriver.Chrome()
 driver.get("https://www.exophase.com/user/"+user+"/")
 
-# Deal with cookies popup
 time.sleep(4)
 iframe = driver.find_element(By.ID,"sp_message_iframe_681693")
 WebDriverWait(driver,10).until(EC.frame_to_be_available_and_switch_to_it(iframe))
 cookieButton = driver.find_element(By.XPATH,'//*[@id="notice"]/div[3]/button[3]')
 WebDriverWait(driver,10).until(EC.element_to_be_clickable(cookieButton)).click()
 
-# Sort game list
 sortButton = driver.find_element(By.XPATH,'//*[@id="dropdownMenuButton"]')
 sortButton.click()
 alphaButton = driver.find_element(By.XPATH,'//*[@id="app"]/div/div[2]/div[1]/div[1]/div[2]/div/div/a[7]')
@@ -125,8 +117,7 @@ while True:
             date = driver.find_element(By.XPATH,date_xpath).text
         except:
             date = ''
-    
-        # Collect platforms
+
         j = 0
         platforms = []
         while True:
@@ -136,8 +127,35 @@ while True:
                 platforms.append(platform)
             except:
                 break
-    
-        # Assigning trophy values
+
+        lib = driver.find_element(By.XPATH,platform_xpath+'/div').get_attribute('class')
+        if lib.startswith('blizzard'):
+            library = 'Blizzard'
+        elif lib.startswith('origin'):
+            library = 'Electronic Arts'
+        elif lib.startswith('epic'):
+            library = 'Epic'
+        elif lib.startswith('gog'):
+            library = 'GOG'
+        elif lib.startswith('android'):
+            library = 'Google Play'
+        elif lib.startswith('nintendo'):
+            library = 'Nintendo'
+        elif lib.startswith('psn'):
+            library = 'PSN'
+        elif lib.startswith('retro'):
+            library = 'Retro'
+        elif lib.startswith('stadia'):
+            library = 'Stadia'
+        elif lib.startswith('steam'):
+            library = 'Steam'
+        elif lib.startswith('uplay'):
+            library = 'Ubisoft'
+        elif lib.startswith('xbox'):
+            library = 'Xbox'
+        else:
+            library = ''
+
         for trophy in trophies:
             if trophies[trophy] == -1:
                 trophies[trophy] = 0
@@ -145,50 +163,43 @@ while True:
                 trophies[trophy] = 1
             else:
                 continue
-        
-        # Cleaning trophy values
+
         if trophies['bronze'] == 0 and trophies['silver'] == 0 and trophies['gold'] == 0 and trophies['platinum'] == 0:
             trophies['bronze'] = ''
             trophies['silver'] = ''
             trophies['gold'] = ''
             trophies['platinum'] = ''
 
-        # Calculating trophy points
         if platform != 'Epic':
             if trophies['bronze'] != '' or trophies['silver'] != '' or trophies['gold'] != '' or trophies['platinum'] != '':
                 points = 15 * int(float(trophies['bronze'])) + 30 * int(float(trophies['silver'])) + 90 * int(float(trophies['gold'])) + 300 * int(float(trophies['platinum']))
 
-        # Cleaning
         region = region[2:]
         hours = hours[:-1]
         achiev_obtained, achiev_total = achievements.split('/')
         date = date.replace(',','')
         platforms = sorted(platforms)
         platforms = ' | '.join(platforms)
-    
-        # Append to dataframe
-        s = pd.Series({'Title':title,'Region':region,'Platforms':platforms,'Hours Played':hours,'Obtained':achiev_obtained,'Total':achiev_total,'Completion':0,'EXP':exp,'Points':points,'Platinums':trophies['platinum'],'Golds':trophies['gold'],'Silvers':trophies['silver'],'Bronzes':trophies['bronze'],'Last Played':date})
+
+        s = pd.Series({'Title':title,'Region':region,'Library':library,'Platforms':platforms,'Hours Played':hours,'Obtained':achiev_obtained,'Total':achiev_total,'Completion':0,'EXP':exp,'Points':points,'Platinums':trophies['platinum'],'Golds':trophies['gold'],'Silvers':trophies['silver'],'Bronzes':trophies['bronze'],'Last Played':date})
         df = df.append(s,ignore_index=True)
         i += 1
-    
+
     except:
         break
 
-# Cleaning
 df = df.drop_duplicates()
 df = df[df['Title'] != 0]
 df = df.sort_values(by=['Title','Region','Platforms'],key=lambda col: col.str.lower())
 
-# Adding completion percentage
 for k in range(len(df)):
-    obtained = int(df.iloc[k,4])
-    total = int(df.iloc[k,5])
+    obtained = int(df.iloc[k,5])
+    total = int(df.iloc[k,6])
     if total == 0:
-        df.iloc[k,6] = ''
+        df.iloc[k,7] = ''
     else:
-        df.iloc[k,6] = round(obtained/total*100,2)
+        df.iloc[k,7] = round(obtained/total*100,2)
 
-# Output to csv
 df.to_csv(r'outputs/exophase_output.csv',encoding='utf-8',index=False)
 
 driver.quit()
